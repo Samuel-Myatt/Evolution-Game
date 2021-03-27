0c4584561d4c4f869b1dd5285d9e9b420705ab79
Charlie-Booth <charliebtf@outlook.com>
2021-03-27 03:14:02 +0000

    test

--- TopDownShooter2D/Assets/Scripts/Entity Scripts/EnemyController.cs
+++ TopDownShooter2D/Assets/Scripts/Entity Scripts/EnemyController.cs
@@ -9,19 +9,11 @@ public class EnemyController : MonoBehaviour
     public float maxHealth = 100f;
 
     public float dmg = 25f;
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
+
 
     public bool dead = false;
     public GameObject thisObject;
-=======
->>>>>>> 99ac46c2195f20f72f4c34396b8825d7b2a4bf80
->>>>>>> Stashed changes
-=======
-    
->>>>>>> parent of 6561b03 (Merge branch 'main' of https://github.com/Samuel-Myatt/Evolution-Game into main)
+
 
 
 
