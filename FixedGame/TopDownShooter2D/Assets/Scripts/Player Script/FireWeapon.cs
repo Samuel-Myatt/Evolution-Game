d547753fea1fe763fc73b788465c18d1f4c34a6d
Sheelp <80409204+Sheelp@users.noreply.github.com>
2021-03-29 17:49:45 +0100

    Multi Bullet added
    Got the multishot feature working. It has a bool to unlock, a bulletAmount float, and a bulletSpread float.

--- FixedGame/TopDownShooter2D/Assets/Scripts/Player Script/FireWeapon.cs
+++ FixedGame/TopDownShooter2D/Assets/Scripts/Player Script/FireWeapon.cs
@@ -24,6 +24,11 @@ public class FireWeapon : MonoBehaviour
     public bool DOTBulletUnlocked = false;
     public float DOTBulletForce = 20f;
 
+    //Multi bullets.
+    public bool multiBulletUnlocked = false;
+    public int multiBulletAmount;
+    public float multiBulletSpread;
+
     void Update()
     {
         if (Input.GetButton("Fire1"))//If Fire1 (mouse 1) is down...
@@ -47,15 +52,42 @@ public class FireWeapon : MonoBehaviour
 
         if (Input.GetButtonDown("Fire2") && DOTBulletUnlocked)//If Fire2 (mouse 2) is pressed...
         {
-            Debug.Log("RightClick");
             Fire(DOTBullet, DOTBulletForce);//DoT bullet.
         }
+
+        if (Input.GetButtonDown("Fire3") && multiBulletUnlocked)//If Fire3 (middle mouse) is pressed...
+        {
+            FireMulti(bullet, bulletForce, multiBulletAmount, multiBulletSpread);//Multi bullets.
+        }
     }
-    void Fire(GameObject bulletType, float forceType)
+    void Fire(GameObject bulletObject, float force)
     {
-        GameObject newBullet = Instantiate(bulletType, weaponFirePoint.position, weaponFirePoint.rotation);
+        GameObject newBullet = Instantiate(bulletObject, weaponFirePoint.position, weaponFirePoint.rotation);
         Rigidbody2D rb = newBullet.GetComponent<Rigidbody2D>();
-        rb.AddForce(weaponFirePoint.up * forceType, ForceMode2D.Impulse);
+        rb.AddForce(weaponFirePoint.up * force, ForceMode2D.Impulse);
+    }
+    void FireMulti(GameObject bulletObject, float force, int amount, float spreadWhole)
+    {
+        if(amount != 1)//Prevent dividing by zero.
+        {
+            float spreadBtwnBullets = spreadWhole / (amount - 1);
+            float startAngle = 90f + weaponFirePoint.rotation.eulerAngles.z + spreadWhole * -0.5f;
+            float angle;
+
+            for (int i = 0; i < amount; i++)
+            {
+                angle = startAngle + spreadBtwnBullets * i;
+
+                GameObject newBullet = Instantiate(bulletObject, weaponFirePoint.position, weaponFirePoint.rotation);
+                Vector3 dir = Quaternion.AngleAxis(angle, Vector3.forward) * Vector3.right;
+                Rigidbody2D rb = newBullet.GetComponent<Rigidbody2D>();
+                rb.AddForce(dir * force, ForceMode2D.Impulse);
+            }
+        }
+        else
+        {
+            Debug.Log("Cannot devide by zero! Single bullet not accepted.");
+        }
     }
     public void FireEnemy()
     {
